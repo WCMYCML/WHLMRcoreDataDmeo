342af62beab799e00cb74dca380263711ceffa37
WCMYCML <443803599@qq.com>
2016-05-21 23:12:51 +0800

    添加了新的数据

---
+++ RecordDemo/RecordDemo/ViewController/YYModelViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYModelViewController.h
+//  RecordDemo
+//
+//  Created by 王浩霖 on 16/5/21.
+//  Copyright © 2016年 wcycml.com.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYModelViewController : UIViewController
+
+@end
---
+++ RecordDemo/RecordDemo/ViewController/YYModelViewController.m
@@ -0,0 +1,33 @@
+//
+//  YYModelViewController.m
+//  RecordDemo
+//
+//  Created by 王浩霖 on 16/5/21.
+//  Copyright © 2016年 wcycml.com.cn. All rights reserved.
+//
+
+#import "YYModelViewController.h"
+
+@interface YYModelViewController ()
+
+@end
+
+@implementation YYModelViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+- (IBAction)addModelS:(UIButton *)sender {
+    
+}
+
+@end
