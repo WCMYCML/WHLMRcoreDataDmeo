373848412d4aa605f43781a6db891abbeccf00fb
WCMYCML <443803599@qq.com>
2016-05-22 18:05:25 +0800

    添加了获取模拟器文件位置的代码

--- RecordDemo/RecordDemo/AppDelegate.m
+++ RecordDemo/RecordDemo/AppDelegate.m
@@ -17,6 +17,23 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    //获取对应路径方便查看数据库等信息
+    NSArray *pathsDocument = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *pathDocument = [pathsDocument objectAtIndex:0];
+    NSLog(@"path: -----》%@", pathDocument);
+    //获取Cache目录
+    NSArray *pathsCache = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *pathCache = [pathsCache objectAtIndex:0];
+    NSLog(@"cache ------->%@", pathCache);
+    //获取Library目录
+    NSArray *pathsLibrary = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *pathLibrary = [pathsLibrary objectAtIndex:0];
+    NSLog(@"library --------->%@", pathLibrary);
+    //获取本应用程序的临时目录
+    NSString *temp = NSTemporaryDirectory();
+    NSLog(@"temp: -----------> %@", temp);
+
+
     [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:@"CordDateOne"];//标准的初始化方法
 
     return YES;
